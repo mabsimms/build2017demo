fa27b1a43ac5cea8a202644c91b2c5fc70b3fde5
Mark Simms <masimms@microsoft.com>
2017-05-02 16:14:27 -0700

    working on sample app implementation

--- src/sample-app/SampleWebApp-Baseline/Support/DataManager.cs
+++ src/sample-app/SampleWebApp-Baseline/Support/DataManager.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SampleWebApp-Baseline
+namespace SampleWebApp.Baseline.Support
 {
-    public class RedisManager
+    public class DataManager
     {
     }
 }
