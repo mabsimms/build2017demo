4634836cbace0fd1156b46790c329833112ff35b
Mark Simms <masimms@microsoft.com>
2017-05-04 11:52:20 -0700

    Going back to non vs tooling

---
+++ src/create-machine.bat
@@ -0,0 +1,6 @@
+docker-machine create -d azure ^
+    --azure-ssh-user masimms ^
+    --azure-subscription-id 3e9c25fc-55b3-4837-9bba-02b6eb204331 ^
+    --azure-open-port 80 ^
+    --azure-open-port 443 ^
+    masbldhst1
---
+++ src/sample-apps/0.baseline/build.bat
@@ -0,0 +1,5 @@
+docker-compose ^
+    -f "C:\code\build2017demo\src\sample-apps\0.baseline\docker-compose.yml" ^
+    -f "C:\code\build2017demo\src\sample-apps\0.baseline\docker-compose.override.yml" ^
+    -f "C:\code\build2017demo\src\sample-apps\0.baseline\docker-compose.vs.debug.yml" ^
+    -p dockercompose1163466910 up
